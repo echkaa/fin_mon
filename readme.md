e1818d5d1a752210d77bcef13cba4a23086c534a
akava <akava@portmone.me>
2022-05-03 00:07:36 +0300

    Added PHPUnit Change in system files Added Readme

---
+++ readme.md
@@ -0,0 +1,6 @@
+***For install project execute next:***
+
+>For PHPUnit:
+
+```cp -i phpunit.xml.dist phpunit.xml```
+
---
+++ src/tests/Functional/TokenTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class TokenTest extends WebTestCase
+{
+    private string $uri = '/v1/token';
+
+    public function testErrorRequest(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request(
+            method: 'GET',
+            uri: $this->uri,
+            parameters: []
+        );
+
+        $this->assertResponseStatusCodeSame(500);
+    }
+
+    public function testSuccessRequest(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request(
+            method: 'GET',
+            uri: $this->uri,
+            parameters: [
+                'client_id' => 'd1befa03c79ca0b84ecc488dea96bc68',
+                'password' => 'mypassword',
+                'username' => 'oleksii_kava',
+                'scope' => 'https://fin_mon.ua/',
+            ]
+        );
+
+        $this->assertResponseStatusCodeSame(200);
+    }
+}
