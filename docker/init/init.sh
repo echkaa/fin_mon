cacfd26c70b01281ae6c7c5507f41bc03862a50c
akava <akava@portmone.me>
2022-08-03 06:22:00 +0300

    Changed init.sh

--- docker/init/init.sh
+++ docker/init/init.sh
@@ -2,11 +2,6 @@
 
 composer install --no-suggest --prefer-dist --optimize-autoloader
 
-mkdir config/jwt
-
-openssl genrsa -out config/jwt/private.pem 4096
-openssl rsa -pubout -in config/jwt/private.pem -out config/jwt/public.pem
-
 chown -R www-data:www-data var
 chown -R www-data:www-data vendor
 
@@ -15,5 +10,8 @@ groupmod --non-unique --gid 1000 www-data
 chown -R www-data:www-data .
 
 php bin/console doctrine:migrations:migrate
-
 php bin/console seed:load user
+
+mkdir config/jwt
+openssl genrsa -out config/jwt/private.pem 4096
+openssl rsa -pubout -in config/jwt/private.pem -out config/jwt/public.pem
