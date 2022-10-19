76d6068926eef69673d0aa7c62f1fa6fcfeb1cb2
akava <akava@portmone.me>
2022-10-19 05:53:47 +0300

    changed docker settings

--- docker/init/init.sh
+++ docker/init/init.sh
@@ -2,9 +2,6 @@
 
 composer install --no-suggest --prefer-dist --optimize-autoloader
 
-chown -R www-data:www-data var
-chown -R www-data:www-data vendor
-
 usermod --non-unique --uid 1000 www-data
 groupmod --non-unique --gid 1000 www-data
 chown -R www-data:www-data .
@@ -13,6 +10,7 @@ echo | php bin/console doctrine:migrations:diff
 echo | php bin/console doctrine:migrations:migrate
 php bin/console seed:load user
 
-mkdir config/jwt
 openssl genrsa -out config/jwt/private.pem 4096
 openssl rsa -pubout -in config/jwt/private.pem -out config/jwt/public.pem
+
+chmod 644 config/jwt/private.pem
